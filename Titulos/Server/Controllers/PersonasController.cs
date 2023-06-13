85b13d3604651ae0a209b747dbc81fa2882aec09
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-06-13 11:39:24 -0300

    Post de persona

---
+++ Titulos/Server/Controllers/PersonasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Titulos.BData.Data;
+using Titulos.BData.Data.Entity;
+
+namespace Titulos.Server.Controllers
+{
+    [ApiController]
+    [Route("api/personas")]
+    public class PersonasController : ControllerBase
+    {
+        private readonly Context context;
+
+        public PersonasController(Context context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Persona>>> Get()
+        {
+            return await context.Personas.ToListAsync();
+        }
+
+
+    }
+}
