e2e41a2f750bb773b7955a6d0e541ca268cf4296
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-06-13 11:06:33 -0300

    CRUD completo

--- Titulos/Server/Controllers/ProfesionesController.cs
+++ Titulos/Server/Controllers/ProfesionesController.cs
@@ -29,5 +29,39 @@ namespace Titulos.Server.Controllers
             await context.SaveChangesAsync();
             return profesion.Id;
         }
+
+        [HttpPut("{id:int}")] // api/profesiones/2
+        public async Task<ActionResult> Put(Profesion profesion, int id )
+        {
+            if (id != profesion.Id)
+            {
+                return BadRequest("El id de la profesion no corresponde.");
+            }
+
+            var existe = await context.Profesiones.AnyAsync(x => x.Id == id);
+            if(!existe)
+            {
+                return NotFound($"La profesión de id={id} no existe");
+            }
+
+            context.Update(profesion);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Profesiones.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound($"La profesión de id={id} no existe");
+            }
+
+            context.Remove(new Profesion() { Id=id });
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
